5acada5477d496c9af295934b391687e3c9fe645
b_babaesh <bazlajter30@gmail.com>
2025-03-29 21:45:37 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}', // Укажи пути к файлам, которые используют Tailwind классы
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
